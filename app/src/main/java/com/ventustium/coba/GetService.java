b9c39cc2e22b9512db730b2d3db1a306545ce0d7
Kevin Luis Linuhung <kevinlinuhung@gmail.com>
2022-10-14 15:27:23 +0700

    Add Comment

--- app/src/main/java/com/ventustium/coba/GetService.java
+++ app/src/main/java/com/ventustium/coba/GetService.java
@@ -8,6 +8,9 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface GetService {
+
+    //masih belum paham menggunakan Retrofit dengan baik.
+    //masih coba-coba. yang penting panggil API nya dulu bisa atau tidak.
     @GET("/laravel/public/api/v1/data/users")
     Call<List<Long>> getData();
 }
--- app/src/main/java/com/ventustium/coba/MainActivity.java
+++ app/src/main/java/com/ventustium/coba/MainActivity.java
@@ -1,35 +1,13 @@
 package com.ventustium.coba;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
+
 import android.widget.TextView;
 
-import com.google.gson.JsonObject;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -38,13 +16,6 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-
-    SwipeRefreshLayout swipeRefreshLayout;
-
-    ExecutorService executor = Executors.newSingleThreadExecutor();
-    Handler handler = new Handler(Looper.getMainLooper());
-
-    String id, name, state, error;
 
     TextView textView;
 
@@ -60,28 +31,20 @@ public class MainActivity extends AppCompatActivity {
                 addConverterFactory(GsonConverterFactory.create()).build();
         GetService api = retrofit.create(GetService.class);
         System.out.println("1");
-        api.getData().enqueue(new Callback<List<Long>>() {
+
+        api.getData().enqueue(new Callback<List<Long>>() { //Saya tidak tau ini betul atau tidak.
             @Override
             public void onResponse(Call<List<Long>> call, Response<List<Long>> response) {
+                //kalau berhasil dipanggil, memberikan respon 2
                 System.out.println("2");
-                String data = String.valueOf(response.body());
-                System.out.println(data);
             }
 
             @Override
             public void onFailure(Call<List<Long>> call, Throwable t) {
+                //jika ada error, akan print Error dan error nya.
+                System.out.println("Error");
                 System.out.println(t);
             }
         });
-//        getData();
-        //disableSSLCertificateChecking();
-
-        swipeRefreshLayout = findViewById(R.id.swipeRefresh);
-        swipeRefreshLayout.setOnRefreshListener(this::getData);
-    }
-
-    private void getData() {
-        swipeRefreshLayout.setRefreshing(false);
-
     }
 }
